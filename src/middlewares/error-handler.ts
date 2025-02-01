b85bb0de2fa0c1fb8c215b9c813368dc738c1262
Prabhat Burnwal <prabhat@valuebound.com>
2025-02-01 18:23:20 +0530

    Update

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 import { CustomError } from '../index'
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
-    if(err instanceof CustomError) {
-        return res.status(err.statusCode).json({ errors: err.serialiseErrors() })
-    }
+export const errorHandler = (
+	err: Error,
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {
+	if(err instanceof CustomError) {
+			return res.status(err.statusCode).json({ errors: err.serialiseErrors() })
+	}
 
-    res.status(500).send({ errors: [ { message: 'something went wrong' } ] })
+    res.status(500).send({
+			errors: [ { message: 'something went wrong' } ]
+		});
 }
