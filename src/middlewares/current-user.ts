e723fede7b9ee5461d1654f29ecb575cafab351c
Prabhat Burnwal <prabhat@valuebound.com>
2025-02-01 18:14:37 +0530

    Update

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,32 +1,33 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken';
 
 declare global {
-	interface Req extends Request {
-		session?: any,
-		currentUser?: any
-	}
+    interface Req extends Request {
+        session?: any,
+        currentUser?: any,
+    }
 }
+
 export const currentUser = (jwt_key: string) => {
-	return (
-		req: Req,
-		res: Response,
-		next: NextFunction
-	) => {
-		if (!req.session?.jwt) {
-			return next();
-		}
+    return (
+        req: Req,
+        res: Response,
+        next: NextFunction
+    ) => {
+        if (!req.session?.jwt) {
+            return next();
+        }
 
-		try {
-			const payload = jwt.verify(
-				req.session.jwt,
-				jwt_key
-			)
-			req.currentUser = payload;
-		} catch (error) {
-			next(error)
-		}
+        try {
+        const payload = jwt.verify(
+            req.session.jwt,
+            jwt_key!
+        )
+        req.currentUser = payload;
+        } catch (err) {
+        next(err)
+        }
 
-		next();
-	}
+        next();
+    }
 }
