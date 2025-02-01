150827d19f4b37d84a6cb629fd8371535c8b4cab
Prabhat Burnwal <prabhat@valuebound.com>
2025-02-01 07:07:41 +0530

    Middlewares and errors exported.

--- middlewares/error-handler.ts
+++ middlewares/error-handler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import { CustomError } from '../'
+import { CustomError } from '../index'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err)
